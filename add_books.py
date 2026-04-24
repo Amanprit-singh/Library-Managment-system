a9afbef3c8c64efef44ef01559b592d343bdf4e0
Amanprit-singh <sardaramanprit@gmail.com>
2026-04-25 01:30:28 +0530

    Update add_books.py

--- add_books.py
+++ add_books.py
@@ -1,6 +1,9 @@
 from utils import books
 
 def add_book():
+    print("\n ADD BOOK")
+    print("-" * 30)
+    
     book_name = input("Enter book name to add:").upper()
     books.append(book_name)
     print(f"Book '{book_name}' added.")
